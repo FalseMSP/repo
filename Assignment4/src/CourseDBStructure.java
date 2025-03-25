ede2bff7cf3581e11858c1923d50f37d799be370
Mark <55561022+FalseMSP@users.noreply.github.com>
2025-03-25 11:11:06 -0400

    added a prime number checking

--- Assignment4/src/CourseDBStructure.java
+++ Assignment4/src/CourseDBStructure.java
@@ -6,7 +6,18 @@ public class CourseDBStructure implements CourseDBStructureInterface {
 	private LinkedList[] hashTable;
     private int tableSize;
     private int size;
-    
+    // isPrime needed for size 
+    public static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
     public CourseDBStructure(int size) {
     	tableSize = size-1;
     	hashTable = new LinkedList[tableSize];
